54b6837634f7f96d423aed64a472b361733aeba1
PjHurtig <>
2023-05-16 08:19:18 +0000

    add addbutton function to create div in user list

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let addBtn = document.getElementById("add-btn");
 let userList = document.getElementsByClassName("user-list");
 let inputGear = document.getElementsByClassName("input-gear");
+
+// add button to add gear to user list from text input
+addBtn.addEventListener("click", function () {
+    let gear = document.createElement("div");
+    userList.appendChild(gear);
+});
