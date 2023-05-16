f179743ea4f1dc7c7a0255124d443555faabb2ed
PjHurtig <>
2023-05-16 09:35:24 +0000

    add delete button

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ let addBtn = document.getElementById("add-btn");
 let userList = document.getElementById("user-list");
 let inputGear = document.getElementById("input-gear");
 
-// add button to add gear to user list from text input
+// adds button to add gear to user list from text input
 addBtn.addEventListener("click", function () {
     let gear = document.createElement("div");
     userList.appendChild(gear);
 
-    // add paragraph with gear item text
+    // adds paragraph with gear item text
     let gearText = document.createElement("p");
     gearText.innerText = inputGear.value;
     gear.appendChild(gearText);
 
-    
+    // adds delete button to gear in user list
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+    gear.appendChild(deleteBtn);
+
+
     // remove text from input after adding
     inputGear.value = "";
 });
