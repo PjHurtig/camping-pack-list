c5c7f886f43de17a781636e2aaa02b5ceb012134
PjHurtig <>
2023-05-18 07:46:00 +0000

    add function to alert the user if trying to add empty input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
     let addBtn = document.getElementById("add-btn");
     let userList = document.getElementById("user-list");
     let inputGear = document.getElementById("input-gear");
-})
+
+    function getItemValue() {
+        const gearText = inputGear.value;
+        if (gearText === "") {
+            alert("No item entered");
+            return false;
+        } else {
+            return gearText;
+        }
+    }
+
+
+
+    function addNewItem() {
+        const newItem = getItemValue();
+        if (newItem !== false) {
+            const gear = document.createElement("div");
+            const gearText = document.createElement("p");
+            gearText.innerText = newItem;
+            gear.appendChild(gearText);
+            userList.appendChild(gear);
+        }
+    }
+
+
+    addBtn.addEventListener("click", addNewItem);
+
+
+});
