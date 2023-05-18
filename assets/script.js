ffe8da0bf212a5f3e3bc9dbd7056a0a4c4055f90
PjHurtig <>
2023-05-18 06:42:00 +0000

    begin refactoring of js code to new file

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let addBtn = document.getElementById("add-btn");
+    let userList = document.getElementById("user-list");
+    let inputGear = document.getElementById("input-gear");
+})
