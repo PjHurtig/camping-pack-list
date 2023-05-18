a6591d8ad94beafa2a60e17d93e98b3c105ed74b
PjHurtig <>
2023-05-18 09:02:56 +0000

    add saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,11 +26,15 @@ document.addEventListener("DOMContentLoaded", function () {
         gearText.contentEditable = true;
     }
 
+    // function for saveBtn, disallow edit text in paragraph
+    function saveItem(gearText) {
+        gearText.contentEditable = false;
+    }
+
     function addButtonsToItem(gear, gearText) {
         // adds delete-button to gear in user list
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "delete";
-
         // delete-button: when clicked, removes gear item from user list
         deleteBtn.addEventListener("click", function () {
             deleteItem(gear);
@@ -38,16 +42,25 @@ document.addEventListener("DOMContentLoaded", function () {
         gear.appendChild(deleteBtn);
 
         // adds edit-button to gear in user list
-        let editBtn = document.createElement("button");
+        const editBtn = document.createElement("button");
         editBtn.textContent = "edit";
         // edit-button: when clicked, makes gear item text editable
         editBtn.addEventListener("click", function () {
             editItem(gearText);
         });
         gear.appendChild(editBtn);
+
+        // adds save-button to save changes made with edit button
+        const saveBtn = document.createElement("button");
+        saveBtn.textContent = "done";
+        // save-button: when clicked makes gear text not editable
+        saveBtn.addEventListener("click", function () {
+            saveItem(gearText);
+        });
+        gear.appendChild(saveBtn);
     }
 
-
+    // adds item div and paragraph with gear item text
     function updateItemList(newItem) {
         const gear = document.createElement("div");
         const gearText = document.createElement("p");
@@ -57,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
         addButtonsToItem(gear, gearText);
     }
 
-    // adds item div and paragraph with gear item text
+    // adds item list with gear item text
     function addNewItem() {
         const newItem = getItemValue();
         if (newItem !== false) {
